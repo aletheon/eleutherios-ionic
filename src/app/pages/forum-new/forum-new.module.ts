85cc364ccb94217f53421669a00689c63c82791e
Rob <naitaubafarm@gmail.com>
2021-02-17 23:02:19 +1300

    changed

--- src/app/pages/forum-new/forum-new.module.ts
+++ src/app/pages/forum-new/forum-new.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ForumNewPageRoutingModule } from './forum-new-routing.module';
 
 import { ForumNewPage } from './forum-new.page';
+import { MomentModule } from 'angular2-moment';
+import { LoadingSpinnerModule } from '../../components';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ForumNewPageRoutingModule
+    ForumNewPageRoutingModule,
+    ReactiveFormsModule,
+    MomentModule,
+    LoadingSpinnerModule
   ],
   declarations: [ForumNewPage]
 })
--- src/app/pages/forum-new/forum-new.page.ts
+++ src/app/pages/forum-new/forum-new.page.ts
@@ -1,14 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { LoadingController, ToastController, AlertController } from '@ionic/angular';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Observable, Subscription, BehaviorSubject, of, from, combineLatest, zip, timer, defer, throwError } from 'rxjs';
+
+import * as _ from "lodash";
+import {
+  SiteTotalService,
+  UserServiceService,
+  UserActivityService,
+  UserForumTagService,
+  UserForumImageService,
+  UserServiceImageService,
+  UserForumServiceBlockService,
+  UserServiceForumBlockService,
+  UserServiceBlockService,
+  UserServiceUserBlockService,
+  UserForumUserBlockService,
+  UserForumRegistrantService,
+  UserTagService,
+  ServiceService,
+  TagService,
+  UserForumService,
+  UserImageService,
+  UserServiceTagService
+} from '../../services';
 
 @Component({
   selector: 'app-forum-new',
   templateUrl: './forum-new.page.html',
   styleUrls: ['./forum-new.page.scss'],
 })
-export class ForumNewPage implements OnInit {
+export class ForumNewPage implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('main-title', { static: false }) titleRef: ElementRef;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private siteTotalService: SiteTotalService,
+    private userActivityService: UserActivityService,
+    private userForumTagService: UserForumTagService,
+    private userForumImageService: UserForumImageService,
+    private userServiceImageService: UserServiceImageService,
+    private userForumServiceBlockService: UserForumServiceBlockService,
+    private userServiceForumBlockService: UserServiceForumBlockService,
+    private userServiceBlockService: UserServiceBlockService,
+    private userServiceUserBlockService: UserServiceUserBlockService,
+    private userForumUserBlockService: UserForumUserBlockService,
+    private userForumRegistrantService: UserForumRegistrantService,
+    private userTagService: UserTagService,
+    private serviceService: ServiceService,
+    private userForumService: UserForumService,
+    private userImageService: UserImageService,
+    private userServiceTagService: UserServiceTagService,
+    private loadingController: LoadingController,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnDestroy () {
+    console.log('ngOnDestroy rob');
+  }
+
+  ngAfterViewInit () {
+    console.log('ngAfterViewInit rob');
+  }
 
   ngOnInit() {
+    const that = this;
+    console.log('ngOnInit rob');
+
   }
+
+  // https://edupala.com/ionic-loading-example/
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad rob');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter rob');
+  }
+
+  ionViewDidEnter () {
+    console.log('ionViewDidEnter rob');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave rob');
+  }
+
+  ionViewWillUnload(){
+    console.log('ionViewWillUnload rob');
+  }
+
+  ionViewWillLeave (){
+    console.log('ionViewWillLeave rob');
+  }
+
 }
